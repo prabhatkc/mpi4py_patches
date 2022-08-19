3e5051e35fd06d5dd75ff389793f0b8b3ebbf193
prabhatkc <prabhat.kc077@gmail.com>
2022-08-19 16:56:40 -0400

    deleted gen_patches.sh (redundant file)

--- gen_patches.sh
+++
@@ -1,4 +0,0 @@
-#for mpi_run
-mpiexec -n 4 python main.py --input-folder './raw_data' --output-fname './results/training_patches.h5' --patch-size 'p55' --mpi_run --rot_augment --ds_augment --air_threshold
-# for serial_run
-# python main.py --input-folder './raw_data' --output-fname './results/training_patches.h5' --patch-size 'p55' --rot_augment --ds_augment --air_threshold
