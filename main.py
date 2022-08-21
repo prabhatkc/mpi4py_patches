4278ef56bcccb3d49f927c2e52ba436929f84a30
prabhatkc <prabhat.kc077@gmail.com>
2022-08-21 15:39:55 -0400

    indentation corrections

--- main.py
+++ main.py
@@ -38,24 +38,24 @@ if __name__ == '__main__':
 	#-------------------------------------------------------------------------------------------------------------------------
 	# in-built additional options
 	#-------------------------------------------------------------------------------------------------------------------------
-	args.scale 						= 1 # for now all low dose and high dose complimentary pairs are of same scale and no upscaling/downscaling is required
-	padding_options					= {'p96':12, 'p75':11, 'p64':4, 'p55':8, 'p42':6, 'p32':4, 'p24':4, 'p12':4}
-	args.lr_padding 				= padding_options[args.patch_size]
-	patch_option 					= {'p96':[84 + args.lr_padding, 84 + args.lr_padding], 'p75':[64 + args.lr_padding, 64 + args.lr_padding], \
+	args.scale                      = 1 # for now all low dose and high dose complimentary pairs are of same scale and no upscaling/downscaling is required
+	padding_options                 = {'p96':12, 'p75':11, 'p64':4, 'p55':8, 'p42':6, 'p32':4, 'p24':4, 'p12':4}
+	args.lr_padding                 = padding_options[args.patch_size]
+	patch_option                    = {'p96':[84 + args.lr_padding, 84 + args.lr_padding], 'p75':[64 + args.lr_padding, 64 + args.lr_padding], \
 									   'p64':[60 + args.lr_padding, 60 + args.lr_padding], \
 									   'p55':[47 + args.lr_padding, 47 + args.lr_padding],  \
 									   'p42':[36 + args.lr_padding, 36 + args.lr_padding], 'p32':[28 + args.lr_padding, 28 + args.lr_padding],\
 									   'p24':[20+args.lr_padding, 20+args.lr_padding], 'p12':[8+args.lr_padding, 8+args.lr_padding]} 
 	args.input_size, args.label_size= patch_option[args.patch_size]
-	args.lr_stride 		   			= int(args.input_size - args.lr_padding)
-	args.target_gen_folder			= "full_3mm_sharp_sorted" #target/high dose folder name
-	args.channel 					= 1
+	args.lr_stride                  = int(args.input_size - args.lr_padding)
+	args.target_gen_folder          = "full_3mm_sharp_sorted" #target/high dose folder name
+	args.channel                    = 1
 	args.shuffle_patches            = False
 	
 	# the array type below is the precision type that MPI performs its operations after the read of data
 	# and up until saving the h5 patches. There is not much flexibity in changing the data type for 
 	# the MPI operations. Have a look at https://pages.tacc.utexas.edu/~eijkhout/pcse/html/mpi-data.html#Python
-	args.dtype						= 'float32' 
+	args.dtype                      = 'float32' 
 
 	# number of random images from each sub-folders used to formulate the h5 training set instead of all the images from
 	# all input-target subfolders
